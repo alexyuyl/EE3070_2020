908d7843d431af9c4a68eafbd3321c4fe22f9c7c
avgrpg <47839191+avgrpg@users.noreply.github.com>
2021-04-01 15:16:24 +0800

    Create sampleDisplay.ino

---
+++ Ben_Repo/sampleDisplay.ino
@@ -0,0 +1,47 @@
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+#define DELAYTIME 500 //0.5s
+                      //at min can be 0.1s
+#define SQUARESIZE 20
+
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup(){
+  Serial.begin(115200);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+  
+  sample();
+}
+
+void loop(){
+  
+}
+
+void sample(){
+  display.clearDisplay(); //init OLED display manual
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Press Key");
+  display.setCursor(0,16);
+  display.println("1:Customer");
+  display.setCursor(0,32);
+  display.println("2:Employee");
+  display.drawLine(0,16,128,16);
+  delay(DELAYTIME);
+}
